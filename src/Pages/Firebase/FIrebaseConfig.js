9032fbc510401a062f63973b54fc4e243cd5f9e0
ashimbhowmik <ashimbhowmik239@gmail.com>
2022-11-08 10:38:01 +0600

    [install css framwork & firebase setup]

---
+++ src/Pages/Firebase/FIrebaseConfig.js
@@ -0,0 +1,22 @@
+// web app's Firebase configuration
+
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//   appId: process.env.REACT_APP_APP_ID,
+// };
+// export default firebaseConfig;
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBFwhHrIhhpI1NxGoyQarGwo5f9zWiXw9E",
+  authDomain: "service-review-project.firebaseapp.com",
+  projectId: "service-review-project",
+  storageBucket: "service-review-project.appspot.com",
+  messagingSenderId: "361179557202",
+  appId: "1:361179557202:web:c0f43b3a5fd22e5f1c1e8b",
+};
+
+export default firebaseConfig;
